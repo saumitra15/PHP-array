8784780fa29987f65031bc6243f9e8d21a1c97cf
Saumitra Bhattacharjee <saumitra@live.ie>
2019-10-31 17:34:20 +0600

    Update string_to_array.php
    Added json encode

--- string_to_array.php
+++ string_to_array.php
@@ -24,8 +24,15 @@ $array = explode(",",$str2);
 // print_r($array); // to view the array
 
 
-/**********************************************************************************************/
+/******************************* CHNAGE ARRAY TO JSON ENCODED VALUE *********************************/
 
+
+$str = "1001,1002,1003,1004,1005,1006,1007,1008,1009,1010";
+// echo $str; // to view the string
+
+// Change string to array
+$array = explode(",",$str);
+// echo json_encode($array); // to view the array
 
 
 
